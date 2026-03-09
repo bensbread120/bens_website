dd5543edd4cb4ce649dcb7fc4508499951377ba7
bensbread120 <bhatfield120@gmail.com>
2026-03-09 20:54:01 +1100

    Delete old code.

--- back-end/src/middleware/upload.middleware.ts
+++
@@ -1,6 +0,0 @@
-import multer from "multer";
-
-export const upload = multer({
-  storage: multer.memoryStorage(), // stores image in memory as a Buffer
-  limits: { fileSize: 5 * 1024 * 1024 }, // max 5MB
-});
--- front-end/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="relative mt-16 mb-5 z-10 text-center text-sm text-white">
-      &copy; {new Date().getFullYear()} Ben Hatfield
-    </footer>
-  )
-}
-
-export default Footer;
